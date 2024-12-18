194a8e4690aba8f173bb9f3bb79ce15f9894c0f8
Lucas-Rojas-Nahuel <rolukasro12@gmail.com>
2024-12-18 17:39:24 -0300

    corrección de importacion en jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,7 +1,7 @@
-const expressjwt = require("express-jwt");
+const {expressjwt} = require("express-jwt");
 
 function authJwt() {
-  const secret = process.env.secret;
+  const secret = process.env.JWT_SECRET;
   const api = process.env.API_URL;
 
   return expressjwt({
