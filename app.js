b6188cadc3213bf620c24747c46b19edcc18d1da
Lucas-Rojas-Nahuel <rolukasro12@gmail.com>
2024-12-18 17:07:53 -0300

    desactivando errorHandler para ver si es el problema

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
 
 
 app.use(authJwt);
-app.use(errorHandler);
+/* app.use(errorHandler); */
 
 app.listen(3001, () => {
   console.log("Server is running on http://localhost:3001");
